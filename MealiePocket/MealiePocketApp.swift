58badac47c0f050ed76a1d7da3aa2f93ebabe062
Loriage <loriage09@gmail.com>
2025-10-20 21:46:25 +0200

    Initial Commit

---
+++ MealiePocket/MealiePocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealiePocketApp.swift
+//  MealiePocket
+//
+//  Created by Loriage on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct MealiePocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
